6db475dc12e17451584585eba4b32037016608e4
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2024-11-11 10:45:52 +0530

    Time: 0 ms (100.00%), Space: 13.8 MB (8.53%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,17 +10,24 @@
  */
 class Solution {
 public:
+    ListNode* reverse(ListNode* &prev, ListNode* &curr){
+        if(curr == NULL){
+            return prev;
+        }
+        ListNode* NextNode = curr -> next;
+        curr -> next = prev;
+      return reverse(curr, NextNode);
+    }
     ListNode* reverseList(ListNode* head) {
         ListNode* prev = NULL;
         ListNode* curr = head;
        
-        while(curr != NULL){
-          ListNode* NewNode = curr -> next;
-           curr -> next = prev;
-            prev = curr;
-            curr = NewNode;
-            
+        // while(curr != NULL){
+        //   ListNode* NewNode = curr -> next;
+        //    curr -> next = prev;
+        //     prev = curr;
+        //     curr = NewNode;
+            return reverse(prev, curr);
 }
-        return prev;
-    }
+        
 };
