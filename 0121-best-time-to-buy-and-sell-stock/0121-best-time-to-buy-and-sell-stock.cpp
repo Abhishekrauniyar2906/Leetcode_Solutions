77936fc13f713a66b0ce69ee57e418b775ed4149
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2024-11-15 00:27:21 +0530

    Time: 0 ms (100.00%), Space: 96.2 MB (25.72%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
-void maxprofitfinder(vector<int>&prices,int i,int &minprice ,int &maxprofit){
-   //Base case
-    if( i == prices.size() ){
-      return ;
-    }
-    //processing means ek case tum  chalo do  baaki recursion sambahl lega 
-    if( prices[i] < minprice )
-
-        minprice = prices[i];
-        
-    
-    int todayprofit = prices[i] - minprice;
-    if( todayprofit > maxprofit )
-        maxprofit = todayprofit;
-    
-    // recursive solution
-    maxprofitfinder( prices, i+1, minprice, maxprofit );
-}
-    int maxProfit(vector<int>& prices) {
+int maxProfit(vector<int>& prices) {
         int minprice = INT_MAX;
         int maxprofit = INT_MIN;
-        maxprofitfinder( prices, 0, minprice, maxprofit );
+        for(int i = 0; i < prices.size(); i++){
+          minprice = min(minprice, prices[i]);
+          maxprofit = max(maxprofit, prices[i] - minprice);
+        }
         return maxprofit;
     }
 };
