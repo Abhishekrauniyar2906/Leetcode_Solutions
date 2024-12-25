83b8f7f22b9860501a7bd3aa29c1f38dce355e60
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2024-12-25 17:49:49 +0530

    Time: 0 ms (100.00%), Space: 16.6 MB (11.82%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -1,61 +1,46 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-  
   int getLength(ListNode* head){
-       ListNode* temp = head;
-     int len = 0;
+         ListNode* temp = head;
+           int count = 0;
     while(temp != NULL){
-       len++;
+        count++;
       temp = temp -> next;
     }
-    
-    return len;
+    return count;
   }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(head == NULL){
-            return head;
-         }
+        if(head == NULL)
+        {
+           return head;
+        }    
+          if(head -> next == NULL){
+               return head;
+          }
       
-      if(head -> next == NULL){
-            return head;
-      }
-      
-      
-      ListNode* prev = NULL;
+          ListNode* prev = NULL;
       ListNode* curr = head;
-      
+      ListNode* NextNode = curr -> next;
       int pos = 0;
       
-      ListNode* nextNode = curr -> next;
-         int len = getLength(head);
-          if(len < k){
-              return head;
-           }
-      while(pos < k){
-       
-        nextNode = curr -> next;
-        curr -> next = prev;
-        prev = curr;
-        curr = nextNode;
-        pos++;
+       int len = getLength(head);
+      if(len < k){
+        return head;
       }
+        while(pos < k){
+                NextNode = curr -> next;
+              curr -> next = prev;
+              prev = curr;
+              curr = NextNode;
+          pos++;
+             }
       
-      ListNode* recursionAns = NULL;
-      if(nextNode  != NULL){
-         recursionAns = reverseKGroup(nextNode, k);
-       head -> next = recursionAns;
-      }
+                ListNode* remaining_Ans = NULL;
       
-      return prev;
-    }
+               if(NextNode != NULL){
+                  remaining_Ans = reverseKGroup(NextNode, k);
+                 head -> next = remaining_Ans;
+              }
+               return prev;
+        }
 };
