feb03be3d352b7b05fc036b53721c41628e18fb9
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-01-29 23:00:23 +0530

    Time: 5 ms (41.03%) | Memory: 11.9 MB (31.37%) - LeetSync

--- 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.cpp
+++ 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.cpp
@@ -1,17 +1,42 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s.size() == 0){
-            return true;
-        }
-        int find = s.find("abc");
-        while(find != string::npos){
-           string left = s.substr(0, find);
-            string right = s.substr(find + 3, s.size());
 
-            return isValid(left + right);
+        stack<char>st;
+        if(s[0] != 'a'){
+            return false;
         }
 
-        return false;
+        for(auto ch : s){
+       if(ch == 'a'){
+        st.push('a');
+       }
+       else if(ch == 'b'){
+        if(!st.empty() && st.top() =='a'){
+            st.push('b');
+        }
+        else{
+            return false;
+        }
+       }
+         else{
+            if(!st.empty() && st.top() == 'b'){
+                st.pop();
+            
+            if(!st.empty() && st.top() == 'a'){
+                st.pop();
+            }
+            else{
+                return false;
+            }
+            }
+            else{
+                return false;
+            }
+         }
+        }
+        
+      return st.empty();
+
     }
 };
