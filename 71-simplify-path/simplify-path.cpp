433113e0564c4c4cc01538eef55c984ee10c9516
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-01-30 23:05:38 +0530

    Time: 3 ms (59.79%) | Memory: 11 MB (62.68%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -1,47 +1,41 @@
 class Solution {
 public:
-
-    void buildAns(stack<string>& st, string &ans) {
-        if (st.empty()) {
-            return;
-        }
-        string minpath = st.top();
-        st.pop();
-        buildAns(st, ans);
-        ans += minpath;
-    }
-
     string simplifyPath(string path) {
-        stack<string> st;
-        int i = 0;
+        stack<string>st;
 
-        while (i < path.size()) {
+        int i = 0;
+        while(i < path.size()){
             int start = i;
             int end = i + 1;
-
-            // Find the next segment of the path
-            while (end < path.size() && path[end] != '/') {
+            while(end < path.size() && path[end] != '/'){
                 ++end;
             }
-
-            string minpath = path.substr(start, end - start);
             i = end;
 
-            if (minpath == "/" || minpath == "/.") {
-                continue;  // Skip redundant slashes or current directory
+            string minpath = path.substr(start, end - start);
+            if(minpath == "/" || minpath == "/."){
+                continue;
             }
-
-            if (minpath == "/..") {
-                if (!st.empty()) {
-                    st.pop();  // Pop the top directory
-                }
-            } else if (minpath != "/") {
-                st.push(minpath);  // Push valid directories onto the stack
+             if(minpath == "/.." ){
+                if(!st.empty())
+                  st.pop();
+            }
+            else{
+                
+                st.push(minpath);
             }
         }
+          vector<string>temp;
+          while(!st.empty()){
+            temp.push_back(st.top());
+            st.pop();
+          }
+          reverse(temp.begin(), temp.end());
+        string ans = "";
 
-        string ans = st.empty() ? "/" : "";
-        buildAns(st, ans);
-        return ans;
+        for(auto ch : temp){
+            ans += ch;
+        }
+        return ans.empty() ? "/" : ans;
     }
 };
