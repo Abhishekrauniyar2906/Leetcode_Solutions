d78684d62592dc3983cbcd33de1619e583874909
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-04-07 10:12:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (99.86%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -1,18 +1,10 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-     if(!p && !q){
+     
+       
+       if(!p && !q){
        return true;
      }
         if(p && q){
@@ -20,6 +12,5 @@ public:
             && isSameTree(p -> left, q -> left) && isSameTree(p -> right, q -> right);  
         }  
         return false;
-       
     }
 };
