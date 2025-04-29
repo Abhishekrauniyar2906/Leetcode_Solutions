426bd6168872b1c4bd105861e16ba3cf34b74059
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-04-29 14:36:09 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (60.78%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,45 +1,38 @@
 class Solution {
 public:
-
-   vector<int> getleftMax(vector<int> &height, int &n){
-    vector<int>left_max(n);
-
-     left_max[0] = height[0];
-
-    for(int i = 1; i < n; i++){
-         left_max[i] = max(left_max[i - 1], height[i]);
-    }
-
-    return left_max;
-   }
-
-     vector<int> getrightMax(vector<int> &height, int &n){
-           vector<int>right_max(n);
-
-           right_max[n - 1] = height[n - 1];
-
-           for(int i = n - 2; i >= 0; i--){
-            right_max[i] = max(right_max[i + 1], height[i]);
-           }
-           return right_max;
-     }
     int trap(vector<int>& height) {
-        int n = height.size();
-       
-        if(n == 0){
+        if(height.size() == 0){
             return 0;
         }
 
-        vector<int>left_max = getleftMax(height, n);
-        vector<int>right_max = getrightMax(height, n);
+        int sum = 0;
+        int left_max = 0;
+        int right_max = 0;
+        int left = 0;
+        int right = height.size() - 1;
 
-        int total_water = 0;
-           
 
-           for(int i = 0; i < n; i++){
-            total_water +=  min(left_max[i], right_max[i]) - height[i];
-           }
+        while(left <= right){
+            if(height[left] <= height[right]){
+                if(height[left] >= left_max){
+                    left_max = height[left];
+                }
+                else{
+                    sum += left_max - height[left];
+                }
+                left++;
+            }
 
-           return total_water;
+            else{
+                if(height[right] >= right_max){
+                    right_max = height[right];
+                }
+                else{
+                    sum += right_max - height[right];
+                }
+                right--;
+            }
+        }
+        return sum;
     }
 };
