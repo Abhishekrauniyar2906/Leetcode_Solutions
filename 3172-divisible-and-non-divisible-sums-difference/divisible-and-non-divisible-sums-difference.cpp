667f998e37a620950052f467037dfd90e0100548
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-05-27 08:53:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (70.35%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        vector<int>ans;
+    //     vector<int>ans;
 
-        for(int i = 1; i <= n; i++){
-           ans.push_back(i);
-        }
-         int sum = 0;
-         vector<int>nums1;
-         vector<int>nums2;
-        for(int i = 0; i < ans.size(); i++){
-            if(ans[i] % m != 0){
-               nums1.push_back(ans[i]);
-            }
-            else{
-                nums2.push_back(ans[i]);
-            }
-        }
-        int sum1 = accumulate(nums1.begin(), nums1.end(), 0);
-        int sum2 = accumulate(nums2.begin(), nums2.end(), 0);
-        return sum1 - sum2;
+    //     for(int i = 1; i <= n; i++){
+    //        ans.push_back(i);
+    //     }
+    //      int sum = 0;
+    //      vector<int>nums1;
+    //      vector<int>nums2;
+    //     for(int i = 0; i < ans.size(); i++){
+    //         if(ans[i] % m != 0){
+    //            nums1.push_back(ans[i]);
+    //         }
+    //         else{
+    //             nums2.push_back(ans[i]);
+    //         }
+    //     }
+    //     int sum1 = accumulate(nums1.begin(), nums1.end(), 0);
+    //     int sum2 = accumulate(nums2.begin(), nums2.end(), 0);
+    //     return sum1 - sum2;
+    // }
+      
+
+        int totalSum = n * (n + 1) / 2;
+        int count = n / m;
+        int divisibleSum = m * count * (count + 1) / 2;
+        return  totalSum - 2 * divisibleSum;
+
     }
 };
