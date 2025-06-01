1047b1e0202793acc0896a830519ac9614978aee
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-06-01 10:22:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (24.18%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,46 +1,43 @@
 class Solution {
 public:
 
-    bool isPossible(vector<int>& nums, int mid , int k, int n){
-         int studentCount = 1;
-        int pageSum = 0;
+  bool isPossible(vector<int> &nums, int mid, int k){
+    int studentcount = 1;
+    int pagesum = 0;
 
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] > mid) return false; // can't allocate this chunk
+    for(int i = 0; i < nums.size(); i++){
+      if(pagesum + nums[i] <= mid){
+        pagesum += nums[i];
+      }
 
-            if(pageSum + nums[i] <= mid) {
-                pageSum += nums[i];
-            } else {
-                studentCount++;
-                pageSum = nums[i];
-
-                if(studentCount > k) return false;
-            }
+      else{
+        studentcount++;
+        if(studentcount > k || nums[i] > mid){
+          return false;
         }
-
-        return true;
+        pagesum = 0;
+        pagesum += nums[i];
+      }
     }
+
+    return true;
+  }
     int splitArray(vector<int>& nums, int k) {
-        int n = nums.size();
-      int s = *max_element(nums.begin(), nums.end());
-         
-         int ans = -1;
+      int s = 0;
+      int sum = accumulate(nums.begin(), nums.end(), 0);
+      int e = sum;
+      int ans = -1;
 
-
-         int e = accumulate(nums.begin(), nums.end(), 0);
-
-         while(s <= e){
-            int mid = s + (e - s) / 2;
-
-            if(isPossible(nums, mid, k, n)){
-                ans = mid;
-                e = mid - 1;
-             }
-             else{
-                s = mid + 1;
-             }
-            
-         }
-         return ans;
+      while(s <= e){
+        int mid = s + (e - s) / 2;
+        if(isPossible(nums, mid, k)){
+          ans = mid;
+          e = mid - 1;
+        }
+        else{
+          s = mid + 1;
+        }
+      }
+      return ans;
     }
 };
