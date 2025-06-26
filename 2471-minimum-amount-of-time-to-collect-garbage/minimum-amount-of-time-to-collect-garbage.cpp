6016681bcb04e72e875bc9d1d8f85f5a014f9fe5
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-06-26 11:09:49 +0530

    Time: 3 ms (97.80%) | Memory: 106.9 MB (99.78%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
@@ -3,27 +3,24 @@ public:
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
         int totalTime = 0;
         int lastM = 0, lastP = 0, lastG = 0;
-        vector<int>travelPrefix(travel.size() + 1, 0);
-
-        for(int i = 0; i < travel.size(); i++){
-          travelPrefix[i + 1] = travelPrefix[i] + travel[i];
-        }
         
         for(int i = 0; i < garbage.size(); i++){
          for(auto ch : garbage[i]){
-          totalTime++;
+         
           if(ch == 'M') lastM = i;
           if(ch == 'P') lastP = i;
           if(ch == 'G') lastG = i;
-          
+           totalTime++;
          
 
          }
         }
 
-        totalTime += travelPrefix[lastM];
-        totalTime += travelPrefix[lastP];
-        totalTime += travelPrefix[lastG];
+        for(int i = 0; i < travel.size(); i++){
+          if(i < lastM) totalTime += travel[i];
+          if(i < lastP) totalTime += travel[i];
+          if(i < lastG) totalTime += travel[i];
+        }
 
         return totalTime;
 
