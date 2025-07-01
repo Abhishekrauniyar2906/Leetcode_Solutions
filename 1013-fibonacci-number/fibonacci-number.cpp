a2084cac5eb4abeebecc1a8ec9093848c4e76c5c
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-07-01 10:50:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (16.76%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int solveUsingTopDown(int n, vector<int> &dp){
-     if(n == 0 || n == 1){
-      return n;
-     }
-      // return the ans which is the valid answer not exists
-      if(dp[n] != -1){
-        return dp[n];
+      
+     dp[0] = 0;
+     if(n == 0){
+        return 0;
       }
+      if(n == 1) return 1;
+     dp[1] = 1;
+      // return the ans which is the valid answer not exists
+      
      // store the ans in dp
-     dp[n] = solveUsingTopDown(n - 1, dp) + solveUsingTopDown(n - 2, dp);
+     for(int i = 2; i <= n; i++){
+     dp[i] = dp[i-1] + dp[i - 2];
+     }
      return dp[n];
     }
     int fib(int n) {
