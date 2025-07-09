595f7219cfc566bdcf72dfff93304fe37cf248eb
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-07-09 11:56:32 +0530

    Time: 95 ms (22.75%) | Memory: 76 MB (20.96%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -5,6 +5,7 @@ public:
         reverse(b.begin(), b.end());
         
         int n = a.size();
+        int m = b.size();
 
         vector<vector<int>>dp(n + 1,  vector<int>(n + 1));
 
@@ -12,12 +13,12 @@ public:
           dp[i][0] = 0;
         }
 
-        for(int j = 0; j <= n; j++){
+        for(int j = 0; j <= m; j++){
           dp[0][j] = 0;
         }
 
         for(int i = 1; i <= n; i++){
-          for(int j = 1; j <= n; j++){
+          for(int j = 1; j <= m; j++){
            if(a[i - 1] == b[j - 1]){
             dp[i][j] = 1 + dp[i - 1][j - 1];
            }
@@ -26,6 +27,6 @@ public:
            }
           }
         }
-        return dp[n][n];
+        return dp[n][m];
     }
 };
