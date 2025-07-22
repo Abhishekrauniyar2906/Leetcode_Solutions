94d8a3f6396c0a7792d41b69de581532c02bfd5a
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-07-22 12:04:09 +0530

    Time: 1633 ms (29.61%) | Memory: 63.1 MB (7.20%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -10,10 +10,11 @@ public:
     }
 
     int solve(string &s, int i, int j, vector<vector<int>>& dp){
-        if(i >= j || isPalindrome(s, i, j)) return 0;
+        if(i >= j) return 0; 
 
         if(dp[i][j] != -1) return dp[i][j];
-
+          
+          if(isPalindrome(s, i, j)) return 0;
         int mini = INT_MAX;
 
         for(int k = i; k < j; k++){
