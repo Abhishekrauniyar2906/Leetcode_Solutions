6cc4caa9c24869efa7e3caeeaa529a5580f553e4
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-08-18 10:18:57 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (98.10%) - LeetSync

---
+++ 274-h-index/h-index.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int s = 0;
+        int e = citations.empty() ? 0 : *max_element(citations.begin(), citations.end());
+        
+        int ans = -1;
+        while(s <= e){
+          int mid = s + (e - s) / 2;
+
+          int count = 0;
+          
+          for(auto &paper : citations){
+            if(paper >= mid){
+              count++;
+            }
+          }
+
+          if(count >= mid){
+            ans = mid;
+            s = mid + 1;
+          }
+          else{
+            e = mid - 1;
+          }
+        }
+        return ans;
+    }
+};
