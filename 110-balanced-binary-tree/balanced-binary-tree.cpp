2268cb50249f3d86192b36000e00334d0264ad13
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-08-26 09:31:59 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (83.94%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -1,51 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-
+    bool is_Balanced = true;
 
     int height(TreeNode* root){
-        if(!root){
-            return 0;
-        }
+      if(!root) return 0;
 
-        int leftheight = height(root -> left);
-        int rightheight = height(root -> right);
-         int ans =   max(leftheight , rightheight) + 1;
-         return ans;
+      int lh = height(root -> left);
+      int rh = height(root -> right);
+      if(is_Balanced && abs(lh - rh) > 1){
+        is_Balanced = false;
+      }
+      return max(lh, rh) + 1;
     }
     bool isBalanced(TreeNode* root) {
-        
-        if(!root){
-            return true;
-        }
-        
-        int left_Height = height(root -> left);
-        int right_Height = height(root -> right);
-         
-         int compare = abs(left_Height - right_Height) <= 1;
-         
-        bool leftAns = isBalanced(root -> left);
-        bool rightAns = isBalanced(root -> right);
-
-        if(compare && leftAns && rightAns) {
-            return true;
-        }
-     
-       else{
-        return false;
-       }
-
-
-
+        height(root);
+        return is_Balanced;
+      
     }
 };
