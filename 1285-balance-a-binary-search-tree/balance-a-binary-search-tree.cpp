b9cc455ed894cb7f64956de6c9465015f6345162
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-09-02 11:41:37 +0530

    Time: 23 ms (26.48%) | Memory: 65.9 MB (26.68%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -1,37 +1,30 @@
+
 class Solution {
 public:
 
-    void inorder(TreeNode* root, vector<int> &in){
-        if(!root){
-            return;
-        }
+    void inorder(TreeNode* root, vector<int> &ans){
+      if(!root) return;
 
-        inorder(root -> left, in);
-        in.push_back(root -> val);
-        inorder(root -> right, in);
+      inorder(root -> left, ans);
+      ans.push_back(root -> val);
+      inorder(root -> right, ans);
+
     }
 
-    TreeNode* buildTree(vector<int> &in, int s, int e){
-        
-        if(s > e){
-            return NULL;
-        }
+    TreeNode* buildTree(vector<int>& in, int s, int e){
+      if(s > e) return NULL;
 
-           int mid = (s + e) >> 1;
-        TreeNode* root = new TreeNode(in[mid]);
+      int mid = s + (e - s) / 2;
 
-        root -> left = buildTree(in, s, mid - 1);
-        root -> right = buildTree(in, mid + 1, e);
-        return root;
+      TreeNode* root = new TreeNode(in[mid]);
+      root -> left = buildTree(in, s, mid - 1);
+      root -> right = buildTree(in, mid + 1, e);
+      return root;
     }
     TreeNode* balanceBST(TreeNode* root) {
         vector<int>in;
-
         inorder(root, in);
-         
-         return buildTree(in, 0, in.size() - 1);
 
-
-
+        return buildTree(in, 0, in.size() - 1);
     }
 };
