d9765952b9bbfb7dbf58b58c0dc5ab4d5de6e116
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-10-09 21:58:17 +0530

    Time: 2 ms (31.17%) | Memory: 22.8 MB (64.85%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -4,15 +4,15 @@ public:
         int n = ratings.size();
         vector<int>candies(n, 1);
 
-        for(int i = 1; i < ratings.size(); i++){
+        for(int i = 1; i < n; i++){
           if(ratings[i] > ratings[i - 1]){
-            candies[i] = candies[i - 1] + 1;
+           candies[i] = candies[i - 1] + 1;
           }
         }
 
         for(int i = n - 2; i >= 0; i--){
           if(ratings[i] > ratings[i + 1]){
-            candies[i] = max(candies[i], candies[i + 1] + 1);
+          candies[i] = max(candies[i], candies[i + 1] + 1);
           }
         }
 
