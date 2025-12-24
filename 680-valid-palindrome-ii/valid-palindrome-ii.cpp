b87e0e2ae7469dbd0649782e081aa94445853eeb
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2025-12-24 11:21:41 +0530

    Time: 0 ms (100.00%) | Memory: 26.6 MB (41.12%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
 
-    bool solve(string &s, int i, int j){
-      while(i <= j){
+    bool solve(string s, int i, int j){
+      while(i < j){
         if(s[i] != s[j]){
           return false;
         }
-         else{
-           i++;j--;
-         }
+        i++;
+        j--;
       }
-
       return true;
     }
     bool validPalindrome(string s) {
         int i = 0;
-      int j = s.size() - 1;
-
-      while(i < j || i <= j){
-        if(s[i] == s[j]){
-        i++;
-        j--;
+        int j = s.size() - 1;
+      
+        while(i < j){
+          if(s[i] == s[j]){
+            i++;
+            j--;
+          }
+          else{
+            // call only once i mean only one deletion is allowed so exit immediately after one call
+            return solve(s, i + 1, j) || solve(s, i, j - 1);
+          }
         }
-         else{
-           return solve(s, i + 1, j) || solve(s, i, j - 1);
-         }
-      }
-
-      return true;
+        return true;
     }
 };
