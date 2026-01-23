19a1946aa2abb9109415788f8048df3e08ba2ced
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-01-23 23:15:33 +0530

    Time: 18 ms (14.36%) | Memory: 11.1 MB (18.72%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char,int> mp;
+        int left = 0;
+        int count = 0;
+        int ans = 0;
+        int maxi  = 0;
+         for(int right = 0; right < s.size(); right++){
+            mp[s[right]]++;
+
+              maxi = max(maxi, mp[s[right]]);
+
+            while((right - left + 1) - maxi > k){
+              mp[s[left]]--;
+              left++;
+
+            }
+
+            ans = max(ans, right - left + 1);
+         }
+         return ans;
+    }
+};
