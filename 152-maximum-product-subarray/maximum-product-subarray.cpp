cc9c4831aa77599f1bd5ca6099d66c04b5212f16
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-01-25 18:31:27 +0530

    Time: 3 ms (14.57%) | Memory: 17.8 MB (23.10%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        
-        
-        int maxi = nums[0];
-        
-        for(int i = 0; i < nums.size(); i++){
-          int prod = 1;
-          for(int j = i; j < nums.size(); j++){
-            prod *= nums[j];
-            maxi = max(maxi, prod);
-          }
-        }
-         
-          
-        return maxi;
+        //We find the maximum product subarray by keeping running products from both directions and updating the maximum product at each step.
+        // suppose we have to find in a line which person is taller than me rather than checking all we just 
+        // walk in a line we can find simply in a line which person is taller than me 
+        //Imagine finding tallest person in a line.
+         //Compare every person with every other person
+// (huge work)
+ //Walk once, keep tallest so far
+
+// Both give same tallest person
+// But effort is VERY different.
+    
+    int n = nums.size();
+     
+     int prefixProd = 1;
+     int suffixProd = 1;
+
+     int maxProd = nums[0];
+
+     for(int i = 0; i < nums.size(); i++){
+      
+      if(prefixProd == 0){
+        prefixProd = 1;
+      }
+
+      if(suffixProd == 0){
+        suffixProd = 1;
+      }
+
+      prefixProd *= nums[i];
+      suffixProd *= nums[n - 1 - i];
+
+      maxProd = max(maxProd, max(suffixProd, prefixProd));
+     }
+     return maxProd;
     }
 };
