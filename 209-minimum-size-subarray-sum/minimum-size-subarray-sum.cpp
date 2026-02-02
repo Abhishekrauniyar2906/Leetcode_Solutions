1d0715da93f7007937b606143c7da3b39ff0b343
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-02-02 14:26:17 +0530

    Time: 0 ms (100.00%) | Memory: 32.1 MB (72.61%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
+        int left = 0;
         int sum = 0;
-          int s = 0;
-          int e = 0;
-          int len = INT_MAX;
+        int mini = INT_MAX;
+        for(int right = 0; right < nums.size(); right++){
+          sum += nums[right];
+
+          while(sum >= target){
+            mini = min(mini, right - left + 1);
+            sum -= nums[left];
+            left++;
+          }
+
           
-        while(e < nums.size()){
-            sum += nums[e];
-        //    if sum == target ho
-        // sum < target ho
-        // Sum > target ho
-           while(sum >= target){
-            len = min(len, e - s + 1);
-            // why remove because we want to explore other it is not valid for the range 
-            sum = sum - nums[s];
-            s++;
-             
-            }
-
-             //loop se bahar matalba, guarantee -> sum < target
-            //explore
-            e++;
-            
         }
-
-        if(len == INT_MAX){
-            return 0;
-        }
-        return len;
+        return mini == INT_MAX ? 0 : mini;
     }
 };
