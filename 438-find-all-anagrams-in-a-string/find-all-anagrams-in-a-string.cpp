1338b21a326a12fc3c0b128f01ac997de51a3041
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-02-02 12:32:06 +0530

    Time: 8 ms (39.03%) | Memory: 11.7 MB (36.61%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int>ans; // to store the indices of first index of anagram value
+        unordered_map<char,int>pMap;
+        unordered_map<char,int>sMap;
+
+        for(auto ch : p){
+          pMap[ch]++;
+        }
+        int count = 0;
+        int left = 0;
+
+        for(int right = 0; right < s.size(); right++){
+          sMap[s[right]]++;
+        // p char is map to the s char of their freq it means count the value 
+          if(sMap[s[right]] <= pMap[s[right]]){
+            count++;
+          }
+
+          // suppose by chance if move the to the right side if current window 
+          // is greater than p size we have to do someting in this case, why p length is < s
+          // and we have to check only p length freq with substring exist or not in s
+
+          if(right - left + 1 > p.size()){
+            //suppose if cbae if we move cbae and cba is valid and right move e then we lost 
+            // a so we have to check this condition
+
+            if(sMap[s[left]] <= pMap[s[left]]){
+               count--; //why because we lost a here and move to e that why count will be decrement
+            }
+            sMap[s[left]]--;
+            left++;
+          }
+          
+          if(count == p.size()){
+            ans.push_back(left);
+          }
+
+        }
+        return ans;
+
+    }
+};
