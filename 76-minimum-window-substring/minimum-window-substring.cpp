d6e09c4ec637b10b5bf2f98a3b31a0279acaefc1
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-02-17 11:10:28 +0530

    Time: 7 ms (63.14%) | Memory: 11.6 MB (43.92%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,55 +1,44 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int len1 = s.length();
-        int len2 = t.length();
-        if(len1 < len2){
-            return "";
-        }
-        int start = 0;
-        int e = 0;
-        int ansIndex = -1;
-        int ansLen = INT_MAX;
-        int count = 0;
-
+        int n = s.size();
+        int m = t.size();
+        
+        if(m > n) return "";
         unordered_map<char,int>sMap;
         unordered_map<char,int>tMap;
-         
-        for (char ch : t) {
-            tMap[ch]++;
+        
+        for(auto ch : t){
+          tMap[ch]++;
         }
-        while(e < s.size()){
+        int left = 0;
+        int ansIndex = 0;
+        int ansLen = INT_MAX;
+        int count = 0;
+        for(int right = 0; right < s.size(); right++){
+           sMap[s[right]]++;
 
-            char ch = s[e]; 
-            sMap[ch]++;
+           if(sMap[s[right]] <= tMap[s[right]]){
+            count++;
+           }
 
-            if(sMap[ch] <= tMap[ch]){
-                count++;
-            }
-             
-             if(count == len2){
-                while(sMap[s[start]] > tMap[s[start]]){
-               sMap[s[start]]--;
-               start++;
-            }
-
-            int windowsize = e - start + 1;
-
-            if(windowsize < ansLen){
-               ansLen = windowsize;
-               ansIndex = start;
-            }
-              
-             
-             }
-
-             e++; 
+           while(count == m){
+            
+          int windowSize = right - left + 1;
+          if(windowSize < ansLen){
+            ansLen = windowSize;
+            ansIndex = left;
           }
+            sMap[s[left]]--;
 
-          if(ansIndex == -1){
-            return "";
-          }
-         
-         return s.substr(ansIndex, ansLen);
+            if(sMap[s[left]] < tMap[s[left]]){
+              count--;
+            }
+            left++;
+           
+          
+           }
+        }
+        return  ansLen == INT_MAX ? "" : s.substr(ansIndex, ansLen);
     }
 };
