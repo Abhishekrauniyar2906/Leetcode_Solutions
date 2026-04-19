272ebacb19b1d337ed4885afbc55bda52d9e9fa6
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-04-19 19:12:39 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (18.10%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    
-    int solve(vector<int> &nums, int start, int end){
-      
-        int prev1 = 0, prev2 = 0;
-        for(int i = start; i <= end; i++){
-            int curr = max(prev1, prev2 + nums[i]);
-            prev2 = prev1;
-            prev1 = curr;
-            
-        }
-        return prev1;
+
+  int solve(vector<int>&nums){
+      int n = nums.size();
+      if(n == 1) return nums[0];
+         vector<int>dp(n);
+      dp[0] = nums[0];
+      dp[1] = max(nums[0], nums[1]);
+      // int ans = 0;
+      for(int i = 2; i < n; i++){
+        int take = nums[i] + dp[i - 2];
+        int notTake = dp[i - 1];
+        dp[i] = max(take, notTake);
+      }
+      return dp[n - 1];
     }
+   
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 0) return 0;
-        if(n == 1) return nums[0];
-
-        int case1 = solve(nums, 0, n - 2);
-        int case2 = solve(nums, 1, n - 1);
-
-        return max(case1, case2);
+       
+      int n = nums.size();
+    
+      if(n == 1)return nums[0];
+      vector<int> temp1(nums.begin(), nums.end() - 1);
+      vector<int> temp2(nums.begin() + 1, nums.end());
+      
+      return max(solve(temp1), solve(temp2));
     }
 };
