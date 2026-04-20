d2015da284fa2f280137e69065aadf87e9b4a2e1
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-04-20 11:16:48 +0530

    Time: 3 ms (19.32%) | Memory: 43.7 MB (15.49%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -1,20 +1,51 @@
 class Solution {
 public:
-  
-       int maxSubarraySumCircular(vector<int>& nums) {
-        int temp_max = 0, temp_min = 0;
-        int max_sum = INT_MIN, min_sum = INT_MAX;
-        int total = 0;
-        for(int i = 0; i < nums.size(); i++){
-            total += nums[i];
-            temp_max += nums[i];
-            temp_min += nums[i];
-            if(temp_max < nums[i]) temp_max = nums[i];
-            if(temp_min > nums[i]) temp_min = nums[i];
-            max_sum = max(max_sum, temp_max);
-            min_sum = min(min_sum, temp_min);
+
+   int maxSum(vector<int>&nums){
+      int n = nums.size();
+      int sum = 0;
+      int maxi = INT_MIN;
+    
+      for(int i = 0; i < n; i++){
+        sum += nums[i];
+        maxi = max(maxi, sum);
+        // mini = min(mini, sum);
+        if(sum < 0){
+          sum = 0;
         }
-        if(total == min_sum) return max_sum;
-        return max(max_sum, total - min_sum);
+      }
+      
+      return maxi;
+    }
+
+      int minSum(vector<int>&nums){
+      int n = nums.size();
+      int sum = 0;
+      // int maxi = INT_MIN;
+      int mini = INT_MAX;
+      for(int i = 0; i < n; i++){
+        sum += nums[i];
+        // maxi = max(maxi, sum);
+        mini = min(mini, sum);
+        if(sum > 0){
+          sum = 0;
+        }
+      }
+      
+      return mini;
+    }
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int LinearSum = maxSum(nums);
+        int minSubarraySum = minSum(nums);
+        if(LinearSum < 0) return LinearSum;
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        
+        int circularSubarraySum = totalSum - minSubarraySum;
+
+        return max(LinearSum, circularSubarraySum);
+        
+
+ 
+
     }
 };
