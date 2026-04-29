a9839f89eb28cc31cfa06954bd300ebad8233037
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-04-29 13:20:31 +0530

    Time: 7 ms (27.73%) | Memory: 27.1 MB (92.23%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,31 +1,30 @@
-
-class Solution {
+class Solution{
 public:
-int index = 0;
+    int index = 0;  
+    TreeNode* solve(vector<int> &inorder, vector<int> &postorder, int start, int end){
+     if(start > end) return NULL;
+     int n = inorder.size();
+      int val = postorder[n - index - 1];
+      index++;
+     
+      // cout <<"the value is : " << val << endl;
     
-   TreeNode* solve(vector<int> &inorder, vector<int> &postorder, int start, int end){
-    if(start > end) return NULL;
-    int n = inorder.size();
-     int data = postorder[n - index - 1];
-     
-     index++;
-     TreeNode* root = new TreeNode(data);
-     int pos = start;
-     for(int i = start; i <= end; i++){
-         if(inorder[i] == data){
-             pos = i;
-             break;
-         }
-     }
-     root -> right = solve(inorder, postorder, pos + 1, end);
-     root -> left = solve(inorder, postorder, start, pos - 1);
-     
-     return root;
-   }
-    TreeNode* buildTree(vector<int> &inorder, vector<int> &postorder) {
-        // code here
-     return solve(inorder, postorder, 0, inorder.size() - 1);
-     
+      TreeNode* root = new TreeNode(val);
+
+      int pos = start;
+      for(int i = start; i <= end; i++){
+        if(inorder[i] == val){
+            pos = i;
+            break;
+        }
+      }
+      
+      
+      root -> right = solve(inorder, postorder,pos + 1, end);
+      root -> left = solve(inorder, postorder,start, pos - 1);
+      return root;
     }
-  
+    TreeNode* buildTree(vector<int> &inorder, vector<int> &postorder) {
+       return solve(inorder, postorder, 0, inorder.size() - 1);
+    }
 };
