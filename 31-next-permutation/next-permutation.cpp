52671456f6879128632b637bc0a60e19b9e282a7
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-05-01 11:35:37 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (67.17%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
+        int circularIndex = -1;
 
-        int circle_index = -1;
-
-        for(int i = n - 1; i > 0; i--){
-            if(nums[i] > nums[i - 1]){
-                circle_index = i - 1;
-                break;
-            }
+        for(int i = nums.size() - 1; i > 0; i--){
+          if(i > 0 && nums[i - 1] < nums[i]){
+            circularIndex = i - 1;
+            break;
+          }
+         
         }
-            if(circle_index == -1){
-                reverse(nums.begin(), nums.end());
-                return;
-            }
-        
-         for(int j = n - 1; j > circle_index; j--){
-            if(nums[j] > nums[circle_index]){
-                swap(nums[j], nums[circle_index]);
-                    break;
-                }
-            }
-             
-             reverse(nums.begin() + circle_index + 1, nums.end());
+        //  cout << "circularIndex :" << circularIndex << endl;
+        if(circularIndex == -1) {
+          reverse(nums.begin(), nums.end());
+          return;
+        }
+
+        for(int i = nums.size() -1; i > circularIndex; i--){
+          if(nums[i] > nums[circularIndex]){
+            swap(nums[i], nums[circularIndex]);
+            break;
+          }
+        }
+        reverse(nums.begin() + circularIndex + 1, nums.end());
     }
 };
