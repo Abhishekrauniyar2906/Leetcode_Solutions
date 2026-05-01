a1c623475aa40b859da29d76a922662be7a200f4
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-05-01 10:34:47 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (47.66%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -1,51 +1,48 @@
 class Solution {
 public:
 
-   int maxSum(vector<int>&nums){
+    int maxSubarrSum(vector<int>&nums){
       int n = nums.size();
       int sum = 0;
       int maxi = INT_MIN;
-    
-      for(int i = 0; i < n; i++){
+
+      for(int i = 0; i < nums.size(); i++){
         sum += nums[i];
         maxi = max(maxi, sum);
-        // mini = min(mini, sum);
         if(sum < 0){
           sum = 0;
         }
       }
-      
       return maxi;
     }
 
-      int minSum(vector<int>&nums){
+
+      int minSubarrSum(vector<int>&nums){
       int n = nums.size();
       int sum = 0;
-      // int maxi = INT_MIN;
       int mini = INT_MAX;
-      for(int i = 0; i < n; i++){
+
+      for(int i = 0; i < nums.size(); i++){
         sum += nums[i];
-        // maxi = max(maxi, sum);
         mini = min(mini, sum);
         if(sum > 0){
           sum = 0;
         }
       }
-      
       return mini;
     }
     int maxSubarraySumCircular(vector<int>& nums) {
-        int LinearSum = maxSum(nums);
-        int minSubarraySum = minSum(nums);
-        if(LinearSum < 0) return LinearSum;
-        int totalSum = accumulate(nums.begin(), nums.end(), 0);
-        
-        int circularSubarraySum = totalSum - minSubarraySum;
+      int totalSum = accumulate(nums.begin(), nums.end(), 0);
 
-        return max(LinearSum, circularSubarraySum);
-        
+      int linearSum = maxSubarrSum(nums);
+       if(linearSum < 0) return linearSum;
+      // cout <<"the linearSum is : " << linearSum << endl;
+      int mini = minSubarrSum(nums);
+      // cout <<"the min sum is : " << mini << endl;
+      int circular = totalSum - mini;
 
- 
+      // cout <<"the circular Sum is : " << circular << endl;
 
+      return max(linearSum, circular);
     }
 };
