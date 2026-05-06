69c9110f879bdba1587bb4657fb7dee9f832c8e2
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-05-06 09:22:45 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (68.01%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -3,23 +3,11 @@ public:
     int singleNonDuplicate(vector<int>& arr) {
         int s = 0;
         int e = arr.size() - 1;
-
-        while(s < e){
-           if(s == e){
-            return s;
-          }
-
+         while(s < e){
+          if(s == e) return s;
           int mid = s + (e - s) / 2;
-          
-          if(mid % 2 == 1){
-            if(arr[mid] == arr[mid - 1]){
-              s = mid + 1;
-            }
-            else{
-              e = mid;
-            }
-          }
-          else{
+    
+          if(mid % 2 == 0){
             if(arr[mid] == arr[mid + 1]){
               s = mid + 1;
             }
@@ -27,9 +15,15 @@ public:
               e = mid;
             }
           }
-         
+          else{
+            if(arr[mid] == arr[mid - 1]){
+              s = mid + 1;
+            }
+            else{
+              e = mid;
+            }
+          }
         }
-        return arr[s];
-
+          return arr[e];
     }
 };
