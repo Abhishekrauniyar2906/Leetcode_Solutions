f5e9ba43b10888d4e780770819a7a9b010749240
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-05-12 11:39:06 +0530

    Time: 0 ms (100.00%) | Memory: 28 MB (44.60%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -1,23 +1,21 @@
 
 class Solution {
 public:
-    
+  int maxi = INT_MIN;
+   int solve(TreeNode* root){
+    if(!root) return 0;
      
-    int solve(TreeNode* root, int &res){
-      if(!root) return 0;
-       
-       // hypothesis
-       int l = max(0,solve(root -> left, res));
-       int r = max(0,solve(root -> right, res));
-        int ans = root -> val + l + r;
-      res = max(res, ans);
-       return root -> val + max(l, r); 
-       }
-     
-    int maxPathSum(TreeNode* root) {
-        int res = INT_MIN;
-         solve(root, res);
-        return res;
+      int left = max(0, solve(root -> left));
+      int right = max(0, solve(root -> right));
+
+      maxi = max(maxi, root -> val + left + right);
       
+      return root -> val + max(left, right);
+   }
+    int maxPathSum(TreeNode* root) {
+     
+
+        solve(root);
+        return maxi;
     }
 };
