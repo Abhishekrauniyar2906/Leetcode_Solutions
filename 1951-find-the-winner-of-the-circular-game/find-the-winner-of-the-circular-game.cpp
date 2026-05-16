71aed7464d7a957201838e009ee1ea6483bd68d5
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-05-16 13:26:17 +0530

    Time: 18 ms (24.14%) | Memory: 26.5 MB (10.37%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-
- 
     int findTheWinner(int n, int k) {
-         queue<int>q;
+      queue<int>q;
+        for(int i = 1; i <= n; i++){
+          q.push(i);
+        }
 
-    //  Initially (means push all the players in the game i mean queue)
-         for(int i = 1; i <= n; i++){
-            q.push(i);
-         }
-
-
-         while(q.size() != 1){
-            for(int i = 1; i < k; i++){
-                int front = q.front();
-                q.pop();
-                q.push(front);
-            }
+        while(q.size() > 1){
+        
+          for(int i = 0; i < k - 1; i++){
+            q.push(q.front());
+            // cout <<"the q.front is : " << q.front() << " ";
             q.pop();
-         }
-
-         return q.front();
+          }
+          q.pop();
+        }
+        return q.front();
     }
 
 };
