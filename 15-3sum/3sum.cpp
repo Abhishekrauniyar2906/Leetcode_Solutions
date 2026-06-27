531265e8b291e9f25949a090e9a2b824eb8aad58
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-06-27 15:52:05 +0530

    Time: 51 ms (44.11%) | Memory: 29 MB (73.15%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,45 +1,44 @@
-      class Solution {
-      public:
-          vector<vector<int>> threeSum(vector<int>& nums) {
-            vector<vector<int>>ans;
-            int n = nums.size();
-            sort(nums.begin(), nums.end());
-
-
-            for(int i = 0; i < nums.size() - 2; i++){
-              int left = i + 1;
-              int right = n - 1;
-              if(i > 0 && nums[i] == nums[i - 1]){
-                continue;
-              }
-
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& arr) {
+  
+vector<vector<int>>ans;
+       
+        // Code Here
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        for(int i = 0; i < n - 2; i++){
+            int left = i + 1;
+            int right = n - 1;
             
-
-              while(left < right){
-                // vector<int>levelans;
-                if(nums[left] + nums[right] == -nums[i]){
-              
-                  ans.push_back({nums[i], nums[left], nums[right]});
-                
-                    while(left < right && nums[left] == nums[left + 1]){
-                    left++;
-                    }
-
-                  while(left < right && nums[right] == nums[right - 1]){
+            if(i > 0 && arr[i - 1] == arr[i]){
+                continue;
+            }
+            
+            while(left < right){
+                if(arr[left] + arr[right] == -arr[i]){
+                   ans.push_back({arr[i], arr[left], arr[right]});
                     
-                right--;
-              }
-                left++;
-                  right--;
+                    while(left < right && arr[left] == arr[left + 1]){
+                        left++;
+                    }
+                    
+                    while(left < right && arr[right] == arr[right - 1]){
+                        right--;
+                    }
+                    left++;
+                    right--;
                 }
-                else if(nums[left] + nums[right] < -nums[i]){
-                  left++;
+                
+                else if(arr[left] + arr[right] < -arr[i]){
+                    left++;
                 }
                 else{
-                  right--;
+                    right--;
                 }
-              }
             }
-            return ans;
-          }
-      };
+            
+        }
+        return ans;
+    }
+};
