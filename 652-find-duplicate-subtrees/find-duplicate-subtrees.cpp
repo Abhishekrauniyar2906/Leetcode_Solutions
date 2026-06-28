d079984ab6c733b672acfde59e83ec482dd869f9
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-06-28 18:31:19 +0530

    Time: 27 ms (31.03%) | Memory: 44.7 MB (31.26%) - LeetSync

--- 652-find-duplicate-subtrees/find-duplicate-subtrees.cpp
+++ 652-find-duplicate-subtrees/find-duplicate-subtrees.cpp
@@ -1,31 +1,26 @@
 
 class Solution {
 public:
+vector<TreeNode*>ans;
 
-    string solve(TreeNode* root, vector<TreeNode*> &result,unordered_map<string,int> &mp){
-        if(!root){
-            return "N";
-        }
+   string solve(TreeNode* root, unordered_map<string,int>&mp){
+    if(!root) return "NULL";
+     
+     string s = to_string(root -> val) + "," + solve(root -> left,mp) + "," + solve(root -> right, mp) + ",";
 
-
-        string s = to_string(root -> val) + "," + solve(root -> left, result, mp) + "," + solve(root -> right, result, mp);
-        
-        if(mp[s] == 1){
-            result.push_back(root);
-        }
-
-        mp[s]++;
-
-        return s;
-
-
-    }
+     if(mp[s] == 1){
+      ans.push_back(root);
+      
+     }
+   
+      mp[s]++;
+     
+     return s;
+   }
     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
       unordered_map<string,int>mp;
+        solve(root, mp);
 
-      vector<TreeNode*> result;
-
-      solve(root, result, mp);
-      return result;  
+        return ans;
     }
 };
