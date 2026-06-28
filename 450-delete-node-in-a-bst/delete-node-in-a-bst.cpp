34ca13e0835837c4f2c172a6615bc35ef18eedbe
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-06-28 18:21:00 +0530

    Time: 0 ms (100.00%) | Memory: 34.4 MB (51.00%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -1,67 +1,53 @@
 
+
+
 class Solution {
 public:
-
-   TreeNode* maxValue(TreeNode* root){
-    if(!root){
-        return NULL;
+    
+   
+    TreeNode* maxValue(TreeNode* root) {
+        // code here
+       
+            while(root -> right != NULL){
+                root = root -> right;
+            }
+        
+        
+        return root;
     }
 
-      while(root -> right != NULL){
-        root = root -> right;
-      }
-
-      return root;
-   }
-    TreeNode* deleteNode(TreeNode* root, int key) {
-         if(!root){
-        return NULL;
-    }
-
-           
-           if(root -> val == key){
-            // case 1 :
-
-            if(root -> left == NULL && root -> right == NULL){
-                return NULL;
-            }
-                //   case 2
-            else if(root -> left == NULL && root -> right != NULL){
-                TreeNode* childSubtree = root -> right;
-                delete root;
-                return childSubtree;
-            }
-             
-
-            //  case 3
-            else if(root -> left != NULL && root -> right == NULL){
-              TreeNode* childSubtree = root -> left;
-                delete root;
-                return childSubtree;
-            }
+    TreeNode* deleteNode(TreeNode* root, int target) {
+               if(!root) return NULL;
           
-        //    case 4
-         
-         else{
-            //root -> left != NULL && root -> right != NULL
-            // it is also known as successor and right side predecessor
-                TreeNode* maxNode = maxValue(root -> left);
-                root -> val = maxNode -> val;
-
-                root -> left = deleteNode(root -> left, maxNode -> val);
-                return root;
-
-         }
-           }
-
-           else if(root -> val > key){
-            root -> left = deleteNode(root -> left, key);
-           }
-
-           else{
-            root -> right = deleteNode(root -> right, key);
-           }
-
-           return root;
+      if(root -> val == target){
+        if(root -> left == NULL && root -> right == NULL){
+          delete root;
+          return NULL;
+        }
+        else if(root -> left != NULL && root -> right == NULL){
+          TreeNode* currentRoot = root -> left;
+          delete root;
+          return currentRoot;
+        }
+        else if(root -> left == NULL && root -> right != NULL){
+          TreeNode* currentRoot = root -> right;
+          delete root;
+          return currentRoot;  
+        }
+        else if(root -> left != NULL && root -> right != NULL){
+          TreeNode* maxNode = maxValue(root -> left);
+          root -> val = maxNode -> val;
+          root -> left = deleteNode(root -> left, maxNode -> val);
+          return root;
+        }
+      }
+        else if(root -> val > target){
+         root -> left = deleteNode(root -> left, target);
+        }
+        else{
+         root -> right = deleteNode(root -> right, target);
+        }
+      
+      return root;
     }
 };
