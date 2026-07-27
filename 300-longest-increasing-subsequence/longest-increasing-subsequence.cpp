2170097aa8aa323a87269332654de2cd0cb7a6fa
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-07-27 13:47:35 +0530

    Time: 3 ms (79.04%) | Memory: 14.1 MB (77.89%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-
-   int solve(vector<int> &nums){
-    int n = nums.size();
-    vector<vector<int>>dp(n + 1 , vector<int>(n + 1, 0));
-
-    for(int curr = n - 1; curr >= 0; curr--){
-      for(int prev = curr - 1; prev >= -1; prev--){
-        int take = 0;
-        if(prev == -1 || nums[curr] > nums[prev])
-        take = 1 + dp[curr + 1][curr + 1];
-
-        int notTake = dp[curr + 1][prev + 1];
-        dp[curr][prev + 1] = max(take, notTake);
-      }
-
-    }
-    return dp[0][0];
-   }
     int lengthOfLIS(vector<int>& nums) {
-        return solve(nums);
+        //sort(nums.begin(), nums.end());
+        vector<int>ans;
+        ans.push_back(nums[0]);
+
+        for(int i = 1; i < nums.size(); i++){
+          if(ans.back() < nums[i]){
+            ans.push_back(nums[i]);
+          }
+          else{
+            auto index = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
+            ans[index] = nums[i];
+          }
+        }
+        return ans.size();
     }
 };
