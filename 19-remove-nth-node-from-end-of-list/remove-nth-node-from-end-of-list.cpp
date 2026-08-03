ce3830583233401800eb4d202f3858606cd0552f
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-08-03 08:57:17 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (93.86%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -1,34 +1,37 @@
 
 class Solution {
 public:
-
     int length(ListNode* head){
       int count = 0;
+
       ListNode* temp = head;
 
-      while(temp != NULL) {
+      while(temp != NULL){
         count++;
         temp = temp -> next;
       }
+      // cout <<"count : " << count << endl;
       return count;
     }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-      if(!head || !head -> next) return NULL;
-        int len = length(head);
-        if(n == len){
-          return head -> next;
-        }
+      if(head == NULL || head -> next == NULL) return NULL;
+      int len = length(head);      
+      if(n == len){
+        return  head -> next;
+      }
 
-        int move = len - n;
-        ListNode* temp = head;
-        for(int i = 1; i < move; i++){
-          temp = temp -> next;
-        }
-        ListNode* deleteNode = temp -> next;
-        temp -> next = deleteNode -> next;
-        deleteNode -> next = NULL;
-        delete deleteNode;
+      int move = len - n;
+      // cout << "the move is: " << move << len <<endl;
+      ListNode* temp = head;
 
-        return head;
+      for(int i = 1; i < move; i++){
+        temp = temp -> next;
+      }
+
+       ListNode* deleteNode = temp -> next;
+       temp -> next = deleteNode -> next;
+       deleteNode -> next = NULL;
+       delete deleteNode;
+       return head;
     }
 };
