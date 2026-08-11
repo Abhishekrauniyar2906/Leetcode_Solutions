94394d6545f61f1a67824e5c14e8e71db71a2b90
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-08-11 13:39:57 +0530

    Time: 16 ms (76.10%) | Memory: 63.2 MB (52.49%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    let left = 0;
+    let ans = 0;
+    let map = new Map();
+    map.set(0, 1)
+    
+    let sum = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        sum += nums[i];
+
+        if(map.has(sum - k)){
+            ans += map.get(sum - k)
+        }
+        map.set(sum, (map.get(sum) || 0) + 1)
+    }
+    return ans;
+};
