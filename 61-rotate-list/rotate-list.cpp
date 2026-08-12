fddc3eb53b3e8787cff993a3670c040ae6e383d5
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-08-12 09:13:32 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (64.84%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -1,41 +1,40 @@
 
 class Solution {
-public: 
+public:
+      int solve(  ListNode*  head){
+          ListNode*  temp  = head;
+          int count = 0;
 
-    int length(ListNode* head){
-      ListNode* temp = head;
-
-      int count = 0;
-      while(temp != NULL){
-        count++;
-        temp = temp -> next;
+          while(temp != NULL){
+            count++;
+            temp =  temp -> next;
+          }
+          return count;
       }
-      return count;
-    }
     ListNode* rotateRight(ListNode* head, int k) {
-      if(!head || !head -> next || k == 0) return head;
-        int n =  length(head);
-        k = k % n;
+        if(!head || !head -> next) return head;
+        int len = solve(head);
+        k = k % len;
         if(k == 0) return head;
-        int move = n - k;
-        
-        ListNode* temp = head;
-        for(int i = 1; i < move; i++){
-          temp = temp -> next;
-        }
-       
-        
-        ListNode* newHead = temp -> next;
-        
-        temp -> next = NULL;
-        ListNode* joinNode = newHead;
-     
-        
-        while(joinNode -> next != NULL){
-          joinNode = joinNode -> next;
-        }
-   
-        joinNode -> next = head;
-        return newHead;
+        int move = len - k;
+
+          ListNode*  temp = head;
+
+          for(int i = 1; i < move; i++){
+            temp = temp -> next;
+          }
+        //   cout <<"temp -> data" << temp -> val << endl;
+
+          ListNode* newHead = temp -> next;
+          ListNode* curr = newHead;
+
+          while(curr -> next != NULL){
+            curr = curr -> next;
+          }
+
+              curr -> next = head;
+              temp -> next = NULL;
+
+              return newHead;
     }
 };
