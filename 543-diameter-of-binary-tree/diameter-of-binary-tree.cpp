08368f009fa1bf7353a1efb74ec6c56210571c65
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-08-12 18:56:30 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (42.82%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -1,23 +1,18 @@
 
 class Solution {
 public:
-   
-    pair<int, int> solve(TreeNode* root){
-      if(!root){
-        pair<int,int> p = make_pair(0, 0);
-        return p;
-      }
 
-      auto l = solve(root -> left);
-      auto r = solve(root -> right);
+    pair<int,int> solve(TreeNode* root){
+        if(!root) return {0, 0};
 
-      int h = max(l.first , r.first) + 1;
-      int temp = l.first + r.first;
-      int d = max({l.second, r.second,  temp});
+        auto l = solve(root -> left);
+        auto r = solve(root -> right);
 
-      return {h, d};
+        int h = max(l.first, r.first) + 1;
+        int d = max(l.first + r.first ,max(l.second, r.second));
+        return {h, d};
     }
     int diameterOfBinaryTree(TreeNode* root) {
-       return solve(root).second;
+        return solve(root).second;
     }
 };
