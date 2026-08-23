031a3f3bc1caa4f15704760cb4c323c7963105be
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-08-23 15:17:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (62.02%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
 
-    void solve(string &digits, vector<string> &mapping, vector<string>&ans, string &output, int index){
-      if(index == digits.size()){
-        ans.push_back(output);
-        return;
-      }
+void solve(string digits, int index, vector<string>&mapping, string &output, vector<string>&ans){
+        if(index >= digits.size()){
+            ans.push_back(output);
+            return;
+        }
 
-      string str = mapping[digits[index] - '0'];
-    //  cout <<"str " << str << endl;
+        string res = mapping[digits[index] - '0'];
 
-      for(int i = 0; i < str.size(); i++){
-//        cout <<"str[i] " << str[i] << endl;
-        output.push_back(str[i]);
-
-        solve(digits, mapping, ans, output, index + 1);
-        output.pop_back();
-      }
+        for(int i = 0; i < res.size(); i++){
+            output.push_back(res[i]);
+            solve(digits, index + 1, mapping, output, ans);
+            output.pop_back();
+        }
     }
     vector<string> letterCombinations(string digits) {
-        vector<string>ans;
-
-        if(digits.empty()) return ans;
-
-        vector<string> mapping = {"", "", "abc" ,"def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
+        vector<string>mapping = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         string output = "";
-       
-       solve(digits, mapping, ans, output, 0);
+        vector<string>ans;
+       solve(digits, 0, mapping, output, ans);
        return ans;
     }
 };
