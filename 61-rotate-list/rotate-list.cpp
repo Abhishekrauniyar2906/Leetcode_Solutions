0b87313dc72206ffb987552455f14cddc941c1d2
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-09-25 11:37:52 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (94.75%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -1,40 +1,38 @@
 
 class Solution {
 public:
-      int solve(  ListNode*  head){
-          ListNode*  temp  = head;
-          int count = 0;
 
-          while(temp != NULL){
-            count++;
-            temp =  temp -> next;
-          }
-          return count;
-      }
-    ListNode* rotateRight(ListNode* head, int k) {
-        if(!head || !head -> next) return head;
-        int len = solve(head);
-        k = k % len;
-        if(k == 0) return head;
-        int move = len - k;
-
-          ListNode*  temp = head;
-
-          for(int i = 1; i < move; i++){
+    int len(ListNode* head){
+        ListNode* temp = head;
+        int c = 0;
+        while(temp != NULL){
+            c++;
             temp = temp -> next;
-          }
-        //   cout <<"temp -> data" << temp -> val << endl;
+        }
+        return c;
+    }
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !head -> next ) return head;
+        int l = len(head);
+        k = k % l;
+        if(k == 0) return head;
+        
+        int move = l - k;
+        // cout << move << endl;
+        ListNode* temp = head;
+        for(int i = 1; i < move; i++){
+            temp = temp -> next;
+        }
+        ListNode* newhead = temp -> next;
+        temp -> next = NULL;
+        // cout << newhead -> val << endl;
+        ListNode* t = newhead;
 
-          ListNode* newHead = temp -> next;
-          ListNode* curr = newHead;
-
-          while(curr -> next != NULL){
-            curr = curr -> next;
-          }
-
-              curr -> next = head;
-              temp -> next = NULL;
-
-              return newHead;
+        while(t -> next != NULL){
+            t = t -> next;
+        }
+        t -> next = head;
+        // cout << t -> val << endl;
+        return newhead;
     }
 };
