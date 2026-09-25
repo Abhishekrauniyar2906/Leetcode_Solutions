e5a746fa0ed904a666a382cbee45dac445afc377
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-09-25 14:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (77.29%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -1,20 +1,26 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-  int maxi = INT_MIN;
-   int solve(TreeNode* root){
-    if(!root) return 0;
-     
-      int left = max(0, solve(root -> left));
-      int right = max(0, solve(root -> right));
+    int maxi = INT_MIN;
+    int solve(TreeNode* root){
+        if(!root) return 0;
+        int l = max(0, solve(root -> left));
+        int r = max(0, solve(root -> right));
 
-      maxi = max(maxi, root -> val + left + right);
-      
-      return root -> val + max(left, right);
-   }
+        maxi = max(maxi, l + r + root -> val);
+        return max(l, r) + root -> val;
+    }
     int maxPathSum(TreeNode* root) {
-     
-
         solve(root);
         return maxi;
     }
