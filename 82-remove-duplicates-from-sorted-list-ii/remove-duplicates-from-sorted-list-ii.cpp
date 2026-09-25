e331527b3291fc9c59a20e7c8998ea3b41de3990
ABHISHEK RAUNIYAR <142793256+Abhishekrauniyar2906@users.noreply.github.com>
2026-09-25 12:05:00 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (6.80%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -2,34 +2,27 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-      if(!head || !head -> next) return head;
         unordered_map<int,int>mp;
 
-        ListNode* temp = head;
+       ListNode* temp = head;
 
-        while(temp != NULL){
-          mp[temp -> val]++;
-          temp = temp -> next;
-        }
+       while(temp != NULL){
+        mp[temp -> val]++;
+        temp = temp -> next;
+       }
 
-        // for(auto x : mp){
-        //   cout << x.first << " -> " << x.second << endl;
-        // }
+         ListNode* dummy = new ListNode(-1);
+         ListNode* ans = dummy;
 
-        temp = head;
-        ListNode* dummy = new ListNode(-1);
-        ListNode* ans = dummy;
-        while(temp != NULL ){
-          // cout <<temp -> val << endl;
-          if(mp[temp -> val] <= 1){
-            
-             ans -> next = temp;
-           
-             ans = temp;
+          temp = head;
+          while(temp != NULL){
+            if(mp[temp -> val] <= 1){
+                ans -> next = temp;
+                ans = temp;
+            }
+            temp = temp -> next;
           }
-          temp = temp -> next;
-        }
-         ans -> next = NULL;
-        return dummy -> next;
+          ans -> next = NULL;
+          return dummy -> next;
     }
 };
